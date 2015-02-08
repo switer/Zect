6be9644c59abd99e9e1c94e1f6840bbb6a02c39a
switer <guankaishe@gmail.com>
2015-02-08 22:06:56 +0800

    z-if  done !

--- dist/zect.js
+++ dist/zect.js
@@ -144,7 +144,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	     *  Directive bindings
 	     */
 	    directives[0] = preset(Zect) // set preset directives
-	    console.log(el)
+
 	    directives.forEach(function (d) {
 	        util.objEach(d, function(id, def) {
 	            parseDirective(vm, id, def)
@@ -160,7 +160,6 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	function parseDirective(vm, id, definition) {
 	    var atn = conf.namespace + id
-
 	    /**
 	     *  using selector to parse declare syntax
 	     */
@@ -208,102 +207,107 @@ return /******/ (function(modules) { // webpackBootstrap
 	}
 
 	function Wrap(nodes) {
-	    var ins = {
-	        find: function(sel) {
-	            var subs = []
-	            nodes.forEach(function(n) {
-	                subs = subs.concat(util.copyArray(n.querySelectorAll(sel)))
-	            })
-	            return Wrap(subs)
-	        },
-	        attr: function(attname, attvalue) {
-	            var len = arguments.length
-	            var el = nodes[0]
-	            if (len > 1) {
-	                el.setAttribute(attname, attvalue)
-	            } else if (len == 1) {
-	                return (el.getAttribute(attname) || '').toString()
-	            }
-	            return this
-	        },
-	        removeAttr: function(attname) {
-	            nodes.forEach(function(el) {
-	                el.removeAttribute(attname)
-	            })
-	            return this
-	        },
-	        addClass: function(clazz) {
-	            nodes.forEach(function(el) {
-	                var classes = util.copyArray(el.classList)
-	                if (!~classes.indexOf(clazz)) classes.push(clazz)
-	                el.className = classes.join(' ')
-	            })
-	            return this
-	        },
-	        removeClass: function(clazz) {
-	            nodes.forEach(function(el) {
-	                el.className = classes.reduce(function(r, n) {
-	                    if (n != clazz) r.push(n)
-	                    return r
-	                }, []).join(' ')
-	            })
-	            return this
-	        },
-	        each: function(fn) {
-	            nodes.forEach(fn)
-	            return this
-	        },
-	        on: function(type, listener, capture) {
-	            nodes.forEach(function(el) {
-	                el.addEventListener(type, listener, capture)
-	            })
-	            return this
-	        },
-	        off: function(type, listener) {
-	            nodes.forEach(function(el) {
-	                el.removeEventListener(type, listener)
-	            })
-	            return this
-	        },
-	        html: function(html) {
-	            var len = arguments.length
-	            if (len >= 1) {
-	                nodes.forEach(function(el) {
-	                    el.innerHTML = html
-	                })
-	            } else if (nodes.length) {
-	                return nodes[0].innerHTML
-	            }
-	            return this
-	        },
-	        parent: function() {
-	            if (!nodes.length) return null
-	            return Wrap([nodes[0].parentNode])
-	        },
-	        remove: function() {
-	            nodes.forEach(function(el) {
-	                var parent = el.parentNode
-	                parent && parent.removeChild(el)
-	            })
-	            return this
-	        },
-	        // return element by index
-	        get: function(i) {
-	            return nodes[i]
-	        },
-	        append: function(n) {
-	            if (nodes.length) this.get(0).appendChild(n)
-	            return this
-	        },
-	        replace: function(n) {
-	            var $parent = this.parent()
-	            $parent.get(0).replaceChild(n, this.get(0))
-	            return this
-	        }
-	    }
-	    ins.__proto__ = Wrap.prototype
-	    return ins
+	    if (nodes instanceof Wrap) return nodes
+	    nodes.__proto__ = proto
+	    return nodes
 	}
+
+	var proto = {
+	    find: function(sel) {
+	        var subs = []
+	        this.forEach(function(n) {
+	            subs = subs.concat(util.copyArray(n.querySelectorAll(sel)))
+	        })
+	        return Wrap(subs)
+	    },
+	    attr: function(attname, attvalue) {
+	        var len = arguments.length
+	        var el = this[0]
+	        if (len > 1) {
+	            el.setAttribute(attname, attvalue)
+	        } else if (len == 1) {
+	            return (el.getAttribute(attname) || '').toString()
+	        }
+	        return this
+	    },
+	    removeAttr: function(attname) {
+	        this.forEach(function(el) {
+	            el.removeAttribute(attname)
+	        })
+	        return this
+	    },
+	    addClass: function(clazz) {
+	        this.forEach(function(el) {
+	            var classes = util.copyArray(el.classList)
+	            if (!~classes.indexOf(clazz)) classes.push(clazz)
+	            el.className = classes.join(' ')
+	        })
+	        return this
+	    },
+	    removeClass: function(clazz) {
+	        this.forEach(function(el) {
+	            el.className = classes.reduce(function(r, n) {
+	                if (n != clazz) r.push(n)
+	                return r
+	            }, []).join(' ')
+	        })
+	        return this
+	    },
+	    each: function(fn) {
+	        this.forEach(fn)
+	        return this
+	    },
+	    on: function(type, listener, capture) {
+	        this.forEach(function(el) {
+	            el.addEventListener(type, listener, capture)
+	        })
+	        return this
+	    },
+	    off: function(type, listener) {
+	        this.forEach(function(el) {
+	            el.removeEventListener(type, listener)
+	        })
+	        return this
+	    },
+	    html: function(html) {
+	        var len = arguments.length
+	        if (len >= 1) {
+	            this.forEach(function(el) {
+	                el.innerHTML = html
+	            })
+	        } else if (this.length) {
+	            return this[0].innerHTML
+	        }
+	        return this
+	    },
+	    parent: function() {
+	        if (!this.length) return null
+	        return Wrap([this[0].parentNode])
+	    },
+	    remove: function() {
+	        this.forEach(function(el) {
+	            var parent = el.parentNode
+	            parent && parent.removeChild(el)
+	        })
+	        return this
+	    },
+	    // return element by index
+	    get: function(i) {
+	        return this[i]
+	    },
+	    append: function(n) {
+	        if (this.length) this.get(0).appendChild(n)
+	        return this
+	    },
+	    replace: function(n) {
+	        var $parent = this.parent()
+	        $parent.get(0).replaceChild(n, this.get(0))
+	        return this
+	    }
+	}
+	proto.__proto__ = Wrap.prototype
+	proto.__proto__.__proto__ = Array.prototype
+
 
 	module.exports = Selector
 
@@ -1583,6 +1587,36 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	module.exports = function(Zect) {
 	    return {
+	        'if': {
+	            bind: function(wkey) {
+	                var parent = this.parent = this.tar.parentNode
+	                this.$holder = document.createComment(conf.namespace + 'if')
+
+	                // insert ref
+	                parent.insertBefore(this.$holder, this.tar)
+	                // parent.removeChild(this.tar)
+
+	                return [wkey]
+	            },
+	            // next: true show || false del
+	            update: function(next) {
+	                var that = this
+	                function mount (tar) {
+	                    that.parent.insertBefore(that.tar, that.$holder)
+	                }
+	                if (!next) {
+	                    this.parent.contains(this.tar) && this.parent.removeChild(this.tar)
+	                } else if (this.childVM) {
+	                    mount()
+	                } else {
+	                    this.childVM = new Zect({
+	                        el: this.tar,
+	                        $data: this.vm.$data
+	                    })
+	                    mount()
+	                }
+	            }
+	        },
 	        'repeat': {
 	            bind: function(wkey) {
 	                var $el = $(this.tar)
@@ -1680,37 +1714,6 @@ return /******/ (function(modules) { // webpackBootstrap
 	            },
 	            update: function(next) {
 	                this.tar.style.display = next ? '' : 'none'
-	            }
-	        },
-	        'if': {
-	            bind: function(wkey) {
-	                var $el = $(this.tar)
-	                var parent = this.parent = this.tar.parentNode
-	                this.$holder = document.createComment(conf.namespace + 'if')
-
-	                // insert ref
-	                parent.insertBefore(this.$holder, this.tar)
-	                parent.removeChild(this.tar)
-
-	                return [wkey]
-	            },
-	            // next: true show || false del
-	            update: function(next) {
-	                var that = this
-	                function mount (tar) {
-	                    that.parent.insertBefore(that.tar, that.$holder)
-	                }
-	                if (!next) {
-	                    this.parent.removeChild(this.tar)
-	                } else if (this.childVM) {
-	                    mount()
-	                } else {
-	                    this.childVM = new Zect({
-	                        el: this.tar,
-	                        $data: this.vm.$data
-	                    })
-	                    mount()
-	                }
 	            }
 	        }
 	    }
