db28ef422a2751b735e48a0e2921bbf5423e6279
switer <guankaishe@gmail.com>
2015-10-13 20:13:38 +0800

    more test case

---
+++ test/spec.directive.js
@@ -0,0 +1,20 @@
+'use strict';
+
+describe('#Directive', function () {
+	it('attr', function () {
+		var app = new Zect({
+			template: '<z-template class="att" z-attr="{attName: attValue}"></z-template>',
+			data: function () {
+				return {
+					attValue: ''
+				}
+			}
+		})	
+		assert(app.$el.hasAttribute('attName'))
+		assert.equal(app.$el.getAttribute('attName'), '')
+		app.$data.attValue = 'v'
+		assert.equal(app.$el.getAttribute('attName'), 'v')
+		app.$data.attValue = null
+		assert.equal(app.$el.getAttribute('attName'), 'null')
+	})
+})
